3c848943838d382341e17408ae09443afd15c927
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2022-05-24 11:40:48 +0530

    stracture updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 
-require('./src/config/database')
+require('./api/config/database')
 const app = express();
 const port =  3005;
 
