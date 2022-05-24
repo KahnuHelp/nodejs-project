15764646eec76f4f809eb71e4a301ec80aad9509
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2022-05-24 11:28:50 +0530

    folder stracture added

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+
+require('./src/config/database')
+const app = express();
+const port =  3005;
+
+//for cors - origin error
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+
+// const router = require('./src/routes/routes'); 
+
+app.use(express.json());
+
+// app.use('/api',router);
+
+
+app.listen(port , ()=>{
+    console.log(`Our node js server started at ${port}`);
+});
